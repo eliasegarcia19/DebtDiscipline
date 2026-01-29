d0c1ccb2e0ec1aabadc381844daf9a110ae8df23
Elias Garcia <eliasegarcia19@gmail.com>
2026-01-28 19:47:42 -0600

    creating react

---
+++ my-react-app/src/Pages/DebtList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DebtList() {
+  return (
+    <div>
+      <h1>Debt List Page</h1>
+      {/* Debt list content goes here */}
+    </div>
+  );
+}
+
+export default DebtList;
